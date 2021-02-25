d0229082cb9192743ca30f0ce8eca439d230801f
fereshte-heidari <74671547+fereshte-heidari@users.noreply.github.com>
2021-02-25 11:07:17 -0800

    submit name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [submit, setSubmit] = useState("");
+  function inputEvent(event) {
+    setName(event.target.value);
+  }
+  function buttonEvent() {
+    setSubmit(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {submit} </h1>
+      <input
+        onChange={inputEvent}
+        value={name}
+        type="text"
+        placeholder="What's your name?"
+      />
+      <button onClick={buttonEvent}>Submit</button>
     </div>
   );
 }
